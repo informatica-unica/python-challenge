ffb3bc4cac93a0e232bf586e0a9378c3dbb2613e
Massimo Bartoletti <bart@unica.it>
2021-02-15 17:59:51 +0100

    added 1.2

---
+++ 1.2/README.md
@@ -0,0 +1,28 @@
+# Esercizio
+
+Disegna una griglia 3x3:
+
+ 
+
+# Comandi
+
+I comandi da usare sono i seguenti.
+
+
+Per creare una nuova tartaruga:
+
+```
+turtle.Turtle()
+```
+
+Per far muovere in avanti la tartaruga di N pixel:
+
+```
+forward(N)
+```
+
+Per ruotare la tartaruga a sinistra di N gradi:
+
+```
+left(N)
+```
---
+++ 1.2/main.py
@@ -0,0 +1,19 @@
+# Il programma disegna un quadrato, in senso antiorario.
+
+import turtle
+
+pippo = turtle.Turtle(); # crea una tartaruga, chiamata pippo
+
+# rettangolo esterno
+pippo.forward(300);
+pippo.left(90);
+pippo.forward(300);
+pippo.left(90);
+pippo.forward(300);
+pippo.left(90);
+pippo.forward(300);
+
+# aggiungere codice qui sotto
+
+
+turtle.done()

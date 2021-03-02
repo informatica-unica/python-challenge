814d40fd045e0e9d871a0d25839893ed61112bd6
Massimo Bartoletti <bart@unica.it>
2021-03-02 16:30:37 +0100

    added input-output

---
+++ basics/input-output/README.md
@@ -0,0 +1,9 @@
+# Input-output
+
+Il programma allegato mostra come leggere dalla tastiera, e come stampare messaggi sulla console. Prova ad eseguire il programma, inserendo da tastiera quanto richiesto, e osservando cosa viene stampato sulla console.
+
+Scrivi un programma che chiede all'utente il suo nome, qual e' la sua pizza preferita, e qual e' il suo dolce preferito, e stampa sulla console un messaggio del tipo:
+
+```
+ciao, Pippo! La tua pizza preferita e' Margherita, mentre il tuo dolce preferito e' Castagnaccio
+```
---
+++ basics/input-output/main.py
@@ -0,0 +1,22 @@
+
+# stampa "come ti chiami" sulla cosule
+print ("come ti chiami? ");
+
+# legge una stringa (sequenza di caratteri) da tastiera
+# e la assegna alla variabile "nome"
+nome = input();
+
+# stampa un messaggio di saluto. L''operatore + concatena due stringhe
+print ("ciao, " + nome);
+
+# stampa un'altra domanda sulla console
+print ("quanti anni hai? ");
+
+# legge una stringa (sequenza di caratteri) da tastiera
+# e la assegna alla variabile "anni"
+anni = input();
+
+# stampa un insulto
+# str(anni) serve per convertire il valore numerico in una stringa
+print (nome + ", hai " + str(anni) + " anni, ma li porti male!");
+

da7bdd30e4ce9830a9604741aec7ef079edce1cb
Massimo Bartoletti <bart@unica.it>
2021-10-07 20:57:21 +0200

    turtle-basic

--- turtle-basics/turtle-demo/main.py
+++ turtle-basics/turtle-demo/main.py
@@ -2,13 +2,14 @@
 # Questi commenti non influenzano in alcun modo l'esecuzione del programma:
 # servono solo come documentazione
 
-# uso le funzioni della libreria turtle
+# qui sto dicendo che voglio usare le funzioni della libreria turtle
 import turtle                
 
 wn = turtle.Screen()
 
 # creo una tartaruga, chiamata pippo
 # il nome della tartaruga non e' importante: scegliete il nome che preferite
+# nello stesso programma posso usare anche piu' tartarughe, con nomi diversi
 
 pippo = turtle.Turtle()
 
@@ -18,39 +19,28 @@ pippo.speed(1)
 
 # il comando forward serve per far avanzare la tartaruga
 # qui sto mandando avanti pippo di 100 unita'
+# (notare che la tartaruga guarda verso destra)
 pippo.forward(300)
 
 # il comando left serve per far ruotare la tartaruga in senso antiorario
 # qui la sto ruotando di 90 gradi
+# (notare che adesso la tartaruga guarda verso l'alto)
 pippo.left(90)
 
 # anziche' un numero, posso usare una espressione aritmetica
 # qui, sto usando l'espressione 2*(100+20), che vale 240
+# (il simbolo asterisco denota la moltiplicazione)
 pippo.forward(2*(100+20))
 
 # il comando right fa l'opposto del comando left:
 # ruota la tartaruga in senso orario 
-# qui sto ruotando pippo di 90 gradi
+# qui sto ruotando pippo di 180 gradi: quindi, sto invertendo la direzione
+# (notare che adesso la tartaruga guarda verso destra)
 pippo.right(90)
 
 # per vedere l'effetto della rotazione, faccio avanzare pippo di 200 unita'
 pippo.forward(200)
 
-# il comando color cambia il colore della tartaruga
-# i nomi dei colori sono in inglese: black, red, green, pink, blue, ...
-# qui sto facendo diventare pippo di colore rosso
-pippo.color("red")
-
-# il comando pensize serve per cambiare la larghezza della linea disegnata
-# qui sto cambiando la dimensione di pippo a 5 
-pippo.pensize(5)
-
-# facendo andare avanti pippo, posso vedere il cambiamento
-pippo.forward(200)
-
 # il comando goto serve per spostare la tartaruga ad una posizione assoluta
+# notare che la tartaruga continua a lasciare la scia
 pippo.goto(0,0)
-
-# aggiungere il codice qui sotto (non modificare le righe sopra questo commento)
-
-wn.exitonclick()

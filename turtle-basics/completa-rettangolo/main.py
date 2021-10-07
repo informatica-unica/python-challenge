3c66deec42ef1ced57e3bac8612f076872356f34
Massimo Bartoletti <bart@unica.it>
2021-10-07 08:28:36 +0200

    completa-rettangolo (part)

---
+++ turtle-basics/completa-rettangolo/main.py
@@ -0,0 +1,15 @@
+# Il programma disegna un quadrato, in senso antiorario.
+
+import turtle
+
+pippo = turtle.Turtle(); # crea una tartaruga, chiamata pippo
+
+pippo.forward(100);
+pippo.left(90);
+pippo.forward(100);
+pippo.left(90);
+pippo.forward(100);
+pippo.left(90);
+pippo.forward(100);
+
+turtle.done()

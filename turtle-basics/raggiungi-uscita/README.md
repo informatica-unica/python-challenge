d367c37a44d371ce09f31d14e0e6ab0c3bb16443
Massimo Bartoletti <bart@unica.it>
2021-10-07 09:14:03 +0200

    raggiungi-uscita

---
+++ turtle-basics/raggiungi-uscita/README.md
@@ -0,0 +1,5 @@
+# Completa il rettangolo
+
+Il frammento di codice allegato disegna due lati di un rettangolo.
+
+Aiuta la tartaruga a completare il rettangolo, tornando al punto di partenza.
---
+++ turtle-basics/raggiungi-uscita/main.py
@@ -0,0 +1,40 @@
+# uso le funzioni della libreria turtle
+import turtle                
+
+# mosse della tartaruga bart (non serve leggere: basta eseguire)
+
+bart = turtle.Turtle()
+bart.color("red")
+bart.pensize(5)
+bart.penup()
+bart.setpos(310,-10)
+bart.pendown()
+bart.right(180)
+bart.forward(10)
+bart.left(90)
+bart.forward(290)
+bart.right(90)
+bart.forward(600)
+bart.right(90)
+bart.forward(600)
+bart.right(90)
+bart.forward(600)
+bart.right(90)
+bart.forward(290)
+bart.left(90)
+bart.forward(10)
+bart.penup()
+bart.setpos(0,-200)
+bart.pendown()
+bart.left(90)
+bart.forward(400)
+
+# mosse della tartaruga pippo
+
+pippo = turtle.Turtle()
+pippo.penup()
+pippo.setpos(-200,0)
+pippo.pendown()
+pippo.forward(20)
+
+# aggiungere il codice qui sotto (non modificare le righe sopra questo commento)

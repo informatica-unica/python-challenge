f3b668da16ca554a9b2919303622844554e787fa
bart <bart@unica.it>
2021-10-08 13:47:16 +0200

    Update main.py

--- turtle-basics/turtle-demo/main.py
+++ turtle-basics/turtle-demo/main.py
@@ -2,39 +2,50 @@
 # Questi commenti non influenzano in alcun modo l'esecuzione del programma:
 # servono solo come documentazione
 
+# Un programma e' formato da una sequenza di comandi,
+# che vengono eseguiti uno dopo l'altro.
+# Esistono modi per cambiare questo flusso di esecuzione,
+# ma li vedremo piu' avanti.
+
 # qui sto dicendo che voglio usare le funzioni della libreria turtle
 import turtle                
 
-# creo una tartaruga, chiamata pippo
-# il nome della tartaruga non e' importante: scegliete il nome che preferite
+# Creo una tartaruga, chiamata pippo
+# Il nome della tartaruga non e' importante: scegliete il nome che preferite
 # nello stesso programma posso usare anche piu' tartarughe, con nomi diversi
 
 pippo = turtle.Turtle()
 
-# il comando speed serve per impostare la velocita' della tartaruga
-# qui la sto impostando a 1, che e' la velocita' piu' bassa
+# Il comando speed serve per impostare la velocita' della tartaruga
+# Qui la sto impostando a 1, che e' la velocita' piu' bassa
+
 pippo.speed(1)
 
-# il comando forward serve per far avanzare la tartaruga
-# qui sto mandando avanti pippo di 300 unita'
+# Il comando forward serve per far avanzare la tartaruga
+# Qui sto mandando avanti pippo di 300 unita'
 # (notare che la tartaruga guarda verso destra)
+
 pippo.forward(300)
 
-# il comando left serve per far ruotare la tartaruga in senso antiorario
-# qui la sto ruotando di 90 gradi
+# Il comando left serve per far ruotare la tartaruga in senso antiorario
+# Qui la sto ruotando di 90 gradi
 # (notare che adesso la tartaruga guarda verso l'alto)
+
 pippo.left(90)
 
-# anziche' un numero, posso usare una espressione aritmetica
-# qui, sto usando l'espressione (2*(100+20)/3)-5, che vale 75
+# Anziche' un numero, posso usare una espressione aritmetica
+# Aui, sto usando l'espressione (2*(100+20)/3)-5, che vale 75
 # (il simbolo asterisco denota la moltiplicazione, / la divisione)
+
 pippo.forward(2*(100+20)/3 - 5)
 
-# il comando right fa l'opposto del comando left:
+# Il comando right fa l'opposto del comando left:
 # ruota la tartaruga in senso orario 
-# qui sto ruotando pippo di 180 gradi: quindi, sto invertendo la direzione
+# Qui sto ruotando pippo di 180 gradi: quindi, sto invertendo la direzione
 # (notare che adesso la tartaruga guarda verso destra)
+
 pippo.right(90)
 
-# per vedere l'effetto della rotazione, faccio avanzare pippo di 150 unita'
+# Per vedere l'effetto della rotazione, faccio avanzare pippo di 150 unita'
+
 pippo.forward(150)

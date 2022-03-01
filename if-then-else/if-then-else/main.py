7389d3142daed65e6d2148973d607e43cda0987e
Massimo Bartoletti <bart@unica.it>
2022-03-01 23:06:52 +0100

    connettivi-logici

--- if-then-else/if-then-else/main.py
+++ if-then-else/if-then-else/main.py
@@ -3,7 +3,7 @@
 # chiedo all'utente di inserire da tastiera un numero minore di 100,
 # e lo memorizzo nella varabile n
 
-n = input("Inserisci un numero minore di 100: ")
+n = int(input("Inserisci un numero minore di 100: "))
 
 # se il numero inserito e' veramente minore di 100, stampo "bravo", 
 # altrimenti, stampo un messaggio di errore.
